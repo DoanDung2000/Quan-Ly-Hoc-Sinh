7302761c9787a8e4b7f3bd12409001071cc1296b
vantien2000 <vantienn740@gmail.com>
2021-06-28 23:07:04 +0700

    Add Form Login

--- QuanLyHocSinh/Login.cs
+++ QuanLyHocSinh/Login.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace QuanLyHocSinh
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Dispose();
         }
     }
 }
